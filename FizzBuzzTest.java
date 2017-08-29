449cfa6781e23e3010c13f9827247f19c9329e53
nramiscal <nramiscal@gmail.com>
2017-08-29 15:47:21 -0400

    Initial commit

---
+++ FizzBuzzTest.java
@@ -0,0 +1,9 @@
+public class FizzBuzzTest {
+  public static void main(String[] args) {
+    FizzBuzz test = new FizzBuzz();
+    test.fizzBuzz(3);
+    test.fizzBuzz(5);
+    test.fizzBuzz(15);
+    test.fizzBuzz(2);
+  }
+}
